28d45a691f41c1a95b1bfb84a29f8f7264649171
stivo12-k <kunangkunang139@gmail.com>
2025-11-23 15:59:08 +0800

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="border-t border-slate-700 bg-slate-800 mt-auto">
+      <div className="container mx-auto px-4 py-4">
+        <div className="text-center text-sm text-slate-400">
+          <p>© 2024 Mobile Legends Heroes</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
